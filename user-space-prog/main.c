6fda82a45e728b22797cac6a9f9e6c6b0058a0dd
emielvdbrink <432064@student.fontys.nl>
2022-03-17 18:07:24 +0100

    added fopen, fwrite, fread and fclose calls

--- user-space-prog/main.c
+++ user-space-prog/main.c
@@ -1,6 +1,30 @@
+/* This is a very simple test program for char devices
+ * It opens the device node and writes/reads the word "yow"
+ * Afterwards it releases the file again.
+ */
+
 #include <stdio.h>
 
 int main(void)
 {
-    printf("hello world!\n");
+    FILE *fp = NULL;
+
+    fp = fopen("/dev/chardev0", "r+");
+
+    if (!fp)
+    {
+        printf("could not open file\n");
+        return -1;
+    }
+
+    char buf[] = "yow";
+
+    printf("write test\n");
+    int  numBytesWritten = fwrite(buf, 4, 1, fp);
+    printf("read test\n");
+    int  numBytesRead    = fread(buf, 4, 1, fp);
+
+    fclose(fp);
+
+    return 0;
 }
