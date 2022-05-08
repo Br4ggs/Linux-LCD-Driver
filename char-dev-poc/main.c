5a8bc0aa146bdb35fcfbe5cd4e07548922a31786
emielvdbrink <432064@student.fontys.nl>
2022-05-08 16:47:15 +0200

    cleanup of main.c and Makefile

--- char-dev-poc/main.c
+++ char-dev-poc/main.c
@@ -39,13 +39,18 @@ MODULE_LICENSE("Dual BSD/GPL");
 #define BCM2835_PERI_BASE 0x20000000
 #define GPIO_BASE         (BCM2835_PERI_BASE + 0x200000)
 
+//GPIO function select registers (offset)
 #define GPFSEL0           0x0
 #define GPFSEL1           0x4
 #define GPFSEL2           0x8
 
+//GPIO set register address (offset)
 #define GPSET0            0x1C
+
+//GPIO clear register address (offset)
 #define GPCLR0            0x28
 
+//Function select register offsets
 #define FSEL2             6
 #define FSEL3             9
 #define FSEL4             12
@@ -58,6 +63,7 @@ MODULE_LICENSE("Dual BSD/GPL");
 #define FSEL24            12
 #define FSEL27            21
 
+//GPIO set & clear register offsets for LCD pins
 #define RS 2
 #define RW 3
 #define E  4
@@ -70,30 +76,9 @@ MODULE_LICENSE("Dual BSD/GPL");
 #define D6 24
 #define D7 27
 
-#define CHAR_A {D6, D0}             //01000001
-#define CHAR_B {D6, D1}             //01000010
-#define CHAR_C {D6, D1, D0}         //01000011
-#define CHAR_D {D6, D2}             //01000100
-#define CHAR_E {D6, D2, D0}         //01000101
-#define CHAR_F {D6, D2, D1}         //01000110
-#define CHAR_G {D6, D2, D1, D0}     //01000111
-#define CHAR_H {D6, D3}             //01001000
-#define CHAR_I {D6, D3, D0}         //01001001
-#define CHAR_J {D6, D3, D1}         //01001010
-#define CHAR_K {D6, D3, D1, D0}     //01001011
-#define CHAR_L {D6, D3, D2}         //01001100
-#define CHAR_M {D6, D3, D2, D0}     //01001101
-#define CHAR_N {D6, D3, D2, D1}     //01001110
-#define CHAR_O {D6, D3, D2, D1, D0} //01001111
-
 //todo: create 12 bit lcd representation
 //todo: create mapper function to translate 12 bit lcd value to gpio set register value (see picture)
 //todo: do this with a switch function
-
-//needs array count
-//maybe make entire function call a macro?
-//test if this works:
-#define PRINT_CHAR_A 
 
 static uint8_t gpioMap[11] = {RS, RW, E, D0, D1, D2, D3, D4, D5, D6, D7};
 
@@ -131,29 +116,6 @@ void clear_display_pins(uint16_t data)
     }
 
     iowrite32(output, gpio + GPCLR0);
-}
-
-void write_to_register(int *bits, int count, void *address)
-{
-    unsigned int output = 0x0;
-
-    int i;
-    for (i = 0; i < count; i++)
-    {
-        output |= (1 << bits[i]);
-    }
-
-    iowrite32(output, address);
-}
-
-void set_output_pins_high(int *pins, int count)
-{
-    write_to_register(pins, count, gpio + GPSET0);
-}
-
-void set_output_pins_low(int *pins, int count)
-{
-    write_to_register(pins, count, gpio + GPCLR0);
 }
 
 int chardev_open(struct inode *inode, struct file *filp)
@@ -320,6 +282,8 @@ static int chardev_init_module(void)
     // set_display_pins(0b00111000000);
     // set_display_pins(0b00111000000);
 
+
+    //See datasheet for info
     //function set
     clear_display_pins(0b11111111111);
     set_display_pins(0b00111000000);
@@ -362,10 +326,13 @@ static int chardev_init_module(void)
 
     mdelay(500); //required?
 
+    //TODO: write some text to screen
+    //"hello world!"
+
     int j;
     for (j = 0; j < 200; j++)
     {
-        //write 'a' to screen?
+        //write 'A' to screen?
         clear_display_pins(0b11111111111);
         mdelay(1);
         set_display_pins(0b01000001001);
@@ -376,15 +343,6 @@ static int chardev_init_module(void)
         clear_display_pins(0b00000000100);
         mdelay(100);
     }
-    // //write 'a' to screen?
-    // clear_display_pins(0b11111111111);
-    // mdelay(1);
-    // set_display_pins(0b01000001001);
-    // clear_display_pins(0b00000000100);
-    // mdelay(1);
-    // set_display_pins(0b00000000100);
-    // mdelay(1);
-    // clear_display_pins(0b00000000100);
 
     return 0;
 
